60fa90fc70e24eebdb9ae0e3bd2c1e92975371a3
dtharpeuno <dtharpe1@gmail.com>
2026-01-08 15:19:15 -0500

    add assets and dirs

---
+++ HPTrivia/HPTrivia/HPTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPTriviaApp.swift
+//  HPTrivia
+//
+//  Created by dtharpeuno on 1/8/26.
+//
+
+import SwiftUI
+
+@main
+struct HPTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
